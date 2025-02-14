26bdba7caf4475b6aa1e3e2eacd0bc81110740ec
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-14 16:03:52 -0300

    feat: Add TypeScript functions for sum, text normalization, and name display

---
+++ ts-function.ts
@@ -0,0 +1,12 @@
+//function
+const soma = (x: number, y: number): number => {
+  return x + y;
+};
+
+const nomrmalizeText = (text: string) => {
+  return text.trim().toLowerCase();
+};
+
+const showName: (name: string) => void = (name: string) => {
+  console.log(name);
+};
