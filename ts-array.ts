feb8385a0d4e93b03484d36b310e974450f5235a
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-14 16:03:24 -0300

    feat: Add TypeScript examples for arrays and tuples

---
+++ ts-array.ts
@@ -0,0 +1,13 @@
+//array
+const arr: Array<number> = [1, 2, 3];
+const arr2: number[] = [1, 2, 3];
+console.log(arr);
+console.log(arr2);
+//tuple
+let tuple: [number, string, boolean] = [1, "jhean", true];
+console.log(tuple);
+//tuple array
+let tupleArr: [number, string, boolean][] = [
+  [1, "jhean", true],
+  [2, "silva", false],
+];
