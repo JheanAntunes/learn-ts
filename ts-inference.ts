a0f8c0ab43157b0b39c4b8227963ba3ded4bc3a9
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-14 16:02:55 -0300

    feat: Add TypeScript inference example with variable declarations and console output

---
+++ ts-inference.ts
@@ -0,0 +1,11 @@
+// const nome:string = 'jhean'
+// const idade:number = 25
+// const adulto:boolean = true
+// const simbolo:symbol = Symbol('qualquer-symbol')
+// console.log(nome, idade, adulto, simbolo)
+const nome: string = "jhean";
+const idade: number = 25;
+const adulto: boolean = true;
+const simbolo: Symbol = Symbol("qualquer-symbol");
+const maiorIdade = idade > 18 ? "adulto" : "menor de idade";
+console.log(nome, idade, adulto, simbolo);
