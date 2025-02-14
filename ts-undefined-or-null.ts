03dc98533fa58093c6ba89f0ef3ca79efa1daf54
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2025-02-14 16:03:41 -0300

    feat: Add TypeScript example demonstrating the difference between undefined and null

---
+++ ts-undefined-or-null.ts
@@ -0,0 +1,9 @@
+// ts value undefined diference null
+var u = undefined; // Definição: Um valor undefined indica que uma variável foi declarada, mas ainda não foi atribuída a ela nenhum valor.
+var n = null; // Definição: O valor null é um valor atribuído explicitamente a uma variável para indicar que ela está intencionalmente vazia ou não possui nenhum valor.
+
+console.log(u);
+console.log(n);
+// Output:
+// undefined
+// null
